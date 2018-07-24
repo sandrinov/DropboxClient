f7ab9c4d95bf4af36ac4ae457ddb8262d02095a1
sandro.vecchiarelli <sandro.vecchiarelli@pomiager.com>
2018-07-24 11:04:09 +0200

    First release - kick off

---
+++ DropBoxClient/Models/FileAndDirViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DropBoxClient.Models
+{
+    public class FileViewModel
+    {
+        public ulong Size { get; set; }
+        public String Name { get; set; }
+    }
+    public class FileUploadViewModel
+    {
+        [Required]
+        public HttpPostedFileBase File { get; set; }
+    }
+    public class DirViewModel
+    {
+        public String Path { get; set; }
+        public String Name { get; set; }
+    }
+}
---
+++ DropBoxClient/Models/UserProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DropBoxClient.Models
+{
+    public class UserProfile
+    {
+        public int ID { get; set; }
+
+        [Required, StringLength(255, MinimumLength = 3)]
+        public string UserName { get; set; }
+
+        public string DropboxAccessToken { get; set; }
+
+        [RegularExpression("^[A-Za-z][A-Za-z'-']*[A-Za-z]$")]
+        public string BlogName { get; set; }
+
+        [StringLength(32)]
+        public string ConnectState { get; set; }
+    }
+
+    public class UsersStore : DbContext
+    {
+        public DbSet<UserProfile> Users { get; set; }
+    }
+}
